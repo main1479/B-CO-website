f760e1a509a0b4613b2a663ebc61717ba8849035
Maruf Mia <mmaruf1137@gmail.com>
2022-09-24 13:46:55 +0600

    update

--- js/main.js
+++ js/main.js
@@ -19,7 +19,40 @@ btnClosenPopup.forEach((el) => {
 });
 
 // =====================
-// nav active
+// nav link and dot link active
 // =====================
 
-// const navLink = document.querySelectorAll('.nav-link');
+const navLink = document.querySelectorAll('.nav-link');
+const dotBtn = document.querySelectorAll('.btn-dot');
+const sections = document.querySelectorAll('.js-section');
+
+function changeLinkActive(target) {
+	const id = target.id;
+	navLink.forEach((el) => {
+		el.classList.remove('active');
+	});
+	dotBtn.forEach((el) => {
+		el.classList.remove('active');
+	});
+
+	const link = document.querySelector(`a.nav-link[href="#${id}"]`);
+	const dotLink = document.querySelector(`a.btn-dot[href="#${id}"]`);
+
+	if (link) {
+		link.classList.add('active');
+	}
+	if (dotLink) {
+		dotLink.classList.add('active');
+	}
+}
+function observeSection(entries, observer) {
+	const [entry] = entries;
+	if (!entry.isIntersecting) return;
+	changeLinkActive(entry.target);
+}
+
+const sectionObesrver = new IntersectionObserver(observeSection, { root: null, threshold: 0.5 });
+
+sections.forEach((section) => {
+	sectionObesrver.observe(section);
+});
