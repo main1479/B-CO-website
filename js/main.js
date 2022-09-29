7ef52db7a2ee3db14a90a09ac46bcd3d89a7c4f2
main1479 <m.main2402@gmail.com>
2022-09-29 20:57:06 +0600

    dots observer fixed

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
+// contact area
+function sendMail() {
+	var params = {
+		from_name: document.getElementById('FullName').value,
+		email_id: document.getElementById('email_id').value,
+		message: document.getElementById('message').value,
+	};
+
+	const serviceID = 'service_gmzsuwe';
+	const templateID = 'template_evzdp95';
+
+	emailjs
+		.send(serviceID, templateID, params)
+		.then((res) => {
+			console.log(res);
+			alert('Your message sent successfully!!');
+		})
+		.catch((err) => console.log(err));
+}
+
 // =====================
 // popup
 // =====================
 const btnOpenPopup = document.querySelectorAll('.btn-marketing');
 const btnClosenPopup = document.querySelectorAll('.btn-close-popup');
+// const btnTarms = document.querySelector('.btn-tarms');
+const footer = document.querySelector('.footer');
 
 btnOpenPopup.forEach((el) => {
 	el.addEventListener('click', (e) => {
@@ -11,10 +33,18 @@ btnOpenPopup.forEach((el) => {
 	});
 });
 
+// btnTarms.addEventListener('click', () => {
+// 	footer.classList.add('show-popup');
+// });
+
 btnClosenPopup.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		const section = e.target.closest('section');
-		section.classList.remove('show-popup');
+		if (section) {
+			section.classList.remove('show-popup');
+		} else {
+			footer.classList.remove('show-popup');
+		}
 	});
 });
 
@@ -27,7 +57,7 @@ const dotBtn = document.querySelectorAll('.btn-dot');
 const sections = document.querySelectorAll('.js-section');
 
 function changeLinkActive(target) {
-	const id = target.id;
+	const id = target.dataset.id || target.id;
 	navLink.forEach((el) => {
 		el.classList.remove('active');
 	});
@@ -36,11 +66,16 @@ function changeLinkActive(target) {
 	});
 
 	const link = document.querySelector(`a.nav-link[href="#${id}"]`);
-	const dotLink = document.querySelector(`a.btn-dot[href="#${id}"]`);
-
 	if (link) {
 		link.classList.add('active');
 	}
+}
+function changeDotActive(target) {
+	const id = target.id;
+	dotBtn.forEach((el) => {
+		el.classList.remove('active');
+	});
+	const dotLink = document.querySelector(`a.btn-dot[href="#${id}"]`);
 	if (dotLink) {
 		dotLink.classList.add('active');
 	}
@@ -49,10 +84,45 @@ function observeSection(entries, observer) {
 	const [entry] = entries;
 	if (!entry.isIntersecting) return;
 	changeLinkActive(entry.target);
+	changeDotActive(entry.target);
 }
 
 const sectionObesrver = new IntersectionObserver(observeSection, { root: null, threshold: 0.5 });
 
 sections.forEach((section) => {
 	sectionObesrver.observe(section);
+});
+
+//  cookie-consent-modal
+
+let cookieModal = document.querySelector('.cookie-consent-modal');
+let cancelCookieBtn = document.querySelector('.Cbtn.cancel');
+let acceptCookieBtn = document.querySelector('.Cbtn.accept');
+
+cancelCookieBtn.addEventListener('click', function () {
+	cookieModal.classList.remove('activeC');
+});
+acceptCookieBtn.addEventListener('click', function () {
+	cookieModal.classList.remove('activeC');
+	localStorage.setItem('cookieAccepted', 'yes');
+});
+
+setTimeout(function () {
+	let cookieAccepted = localStorage.getItem('cookieAccepted');
+	if (cookieAccepted != 'yes') {
+		cookieModal.classList.add('activeC');
+	}
+}, 2000);
+
+// scroll down btn
+const scrollDownBtn = document.querySelectorAll('.scroll-down-btn');
+
+scrollDownBtn.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		scrollDownBtn.forEach((item) => {
+			item.classList.remove('active');
+		});
+		const targetBtn = e.target;
+		targetBtn.classList.add('active');
+	});
 });
